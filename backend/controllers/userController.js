ba6a8d56601d941a5039ccf66e513ac1334df046
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-11-25 17:15:31 +0530

    User Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -83,7 +83,12 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Public
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).send("Auth user");
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 // @desc    Get user profile
--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -5,20 +5,22 @@ import User from "../models/userModel.js";
 const protect = asyncHandler(async (req, res, next) => {
   let token;
   token = req.cookies.jwt;
+
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await User.findOne(decoded.userId).select("-password");
+      req.user = await User.findById(decoded.userId).select("-password");
 
       next();
     } catch (error) {
+      console.error(error);
       res.status(401);
-      throw new Error("Invalid JWT token");
+      throw new Error("Not authorized, token failed");
     }
   } else {
     res.status(401);
-    throw new Error("Not authorized, no token found");
+    throw new Error("Not authorized, no token");
   }
 });
 
