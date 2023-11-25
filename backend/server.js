914141baa21d0a264a81c665ce7aaaa8860f6abf
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-11-25 17:06:09 +0530

    Auth Middleware for protected routes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
